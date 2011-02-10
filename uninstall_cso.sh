8be83fd4c010307dd0432935e683542cc95ece7a
Jérôme Gazel <jerome.gazel@centraliens-nantes.net>
2011-02-10 22:50:34 +0100

    Suppression des fichiers d'installation et de désinstallation CSO

--- uninstall_cso.sh
+++
@@ -1,8 +0,0 @@
-rm -fr ~/Scala
-rm -fr ~/ScalaLocal
-rm antlib.xml build.xml NOTICE.txt README.txt README.txt~~ ARTISTIC.txt PRELUDE.txt  README.txt~ 
-rm -fr src
-rm -fr CSwing 
-sed -i".bak" '/export PATH=$PATH:${SCALA_HOME}\/bin/d' ~/.bashrc 
-sed -i".bak" '/export SCALA_HOME="~\/Scala/d' ~/.bashrc     
-
