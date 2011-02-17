0c6aa8713ebdd2a743b7703a51b0eb8b0a52c137
Clément Schiano de Colella <clement@clement.(none)>
2011-02-17 13:58:38 +0100

    Beaux gosses Helloworld en cso fonctionnel

--- CSO/HelloWorld/src/Hello_CSO.scala
+++ CSO/HelloWorld/src/Hello_CSO.scala
@@ -2,21 +2,25 @@ package jerome_cso
 import ox.CSO._
 
 object Hello_CSO {
-    val n = 3
-
     def main( args: Array[String] ) {
-        say("Hello_CSOprogram")
+     
+    ( Hello_CSO.Hello
+     || 
+      Hello_CSO.World
+    )()
+
+    }
+
+
+    def Hello( ) : PROC={
+	say("Hello")
+    }
+
+    def  World( ) : PROC={
+	say("World")
     }
 
     def say( word: String ) {
       println(word)
-    }
-    
-    
-    
-    trait Message {}
-    case object Ping extends Message {}
-    case class Data[T] ( data: T ) extends Message {}
-    
-    
+    }  
 }
