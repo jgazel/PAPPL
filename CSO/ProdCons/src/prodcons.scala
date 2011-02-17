f3ce432dea1ea0cecc4e2377043bc73f2e554229
Jérôme Gazel <jerome.gazel@centraliens-nantes.net>
2011-02-17 19:46:50 +0100

    Producteurs-Consommateurs qui compile. Par contre la boucle tourne dans le vide et rien ne s'affiche.. marche, marche pas ?

--- CSO/ProdCons/src/prodcons.scala
+++ CSO/ProdCons/src/prodcons.scala
@@ -10,42 +10,55 @@ object ProdCons {
     val left, mon, right = OneOne[String]
 
     def main( args: Array[String] ) {
-        (|| (for (i <- 0 until NP) yield Producteur(i, left) )
-         || (for (i <- 0 until NC) yield Consommateur(i, right) )
-         || Buffer(mon) 
+        
+        println("Début du programme Producteur-Consommateur")
+        ( 
+           || ( for (i <- 0 until NP) yield Producteur(i, left) )
+        || 
+           || ( for (i <- 0 until NC) yield Consommateur(i, right) )
+        || 
+           Buffer(mon) 
         )()
     }
 
     def Producteur(i : Int, out : ![String]) : PROC = {
         var nb = 0
         var message = ""
-
-            repeat(enVie) {
-                message = "Objet numéro " + nb
-                out ! message
-                println("Production " + i + " : " + message)
-                nb = nb + 1
-            }
+        println("Création d'un producteur (" + i + ") :: enVie? " + enVie)
+        
+        repeat(enVie) {
+            println("   Boucle Producteur -- Objet numéro " + nb)
+            sleep(i)
+            message = "Objet numéro " + nb
+            println(message)
+            out ! message
+            println("Production " + i + " : " + message)
+            nb = nb + 1
+        }
     }
 
     def Consommateur(i : Int, in : ?[String]) : PROC = {
         var k = 0
+        println("Création d'un consommateur (" + i + ") :: enVie? " + enVie)
         
-            repeat(enVie) {
-                in ? { message => 
-                        {
-                            mon ! message 
-                            right! message 
-                            println("Consommateur " + i + " : " + message)
-                        } 
-                    }
-                //sleep(500*i)
-            }
+        repeat(enVie) {
+            println("   Boucle Consommateur")
+            in ? { message => 
+                    {
+                        mon ! message 
+                        right! message 
+                        println("Consommateur " + i + " : " + message)
+                    } 
+                }
+            //sleep(500*i)
+        }
     }  
     
 
     def  Buffer( tuyau : ?[String] ) : PROC = {
-         tuyau ? { v => {println(v)}}
+        println("Entrée Buffer")
+        tuyau ? { v => {println(v)}}
     }
+    
 }
 
