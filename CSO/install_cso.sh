488b34c2824beb9caf13a5e1e9a86a63eb5e18a0
Jérôme Gazel <jerome.gazel@centraliens-nantes.net>
2011-02-17 13:31:28 +0100

    Modification pour généralisation de l'installation

--- CSO/install_cso.sh
+++ CSO/install_cso.sh
@@ -11,7 +11,6 @@ mv scala-2.8.1.final ~/Scala
 echo 'export SCALA_HOME="~/Scala"' >> ~/.bashrc
 echo 'export PATH=$PATH:${SCALA_HOME}/bin' >> ~/.bashrc
 
-
 mkdir ~/ScalaLocal
 
 wget http://users.comlab.ox.ac.uk/bernard.sufrin/CSO/cso-sources-scala2.8.0.tgz
@@ -23,3 +22,8 @@ mv scalatasks.xml ~/ScalaLocal/
 source ~/.bashrc
 scala -version
 ant jar
+
+cp cso.jar ~/ScalaLocal
+cp cswing.jar ~/ScalaLocal
+
+echo "Installation CSO Terminée"
