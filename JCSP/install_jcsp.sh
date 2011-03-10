cb5f97b8f13803e31a8f7058586bcd492cd360bf
Clément Schiano de Colella <clement@ubuntu.home>
2011-03-10 09:57:21 +0100

    Script d'installation de JCSP

---
+++ JCSP/install_jcsp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script pour installer JCSP sur une machine sous GNU/Linux
+
+sudo apt-get install openjdk-6-jdk
+wget http://www.cs.kent.ac.uk/projects/ofa/jcsp/jcsp-1.1-rc4.jar
+jar xf jcsp-1.1-rc4.jar
+
+echo "Installation JCSP terminée"
