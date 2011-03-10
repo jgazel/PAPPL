deafb67b3f1e1d9b8fd2082749eb9aa56c917a43
Clément Schiano de Colella <clement@ubuntu.home>
2011-03-10 11:11:45 +0100

    Script d'installation actualisé avec export du classpath. Mais ça ne compile tjs pas

--- JCSP/install_jcsp.sh
+++ JCSP/install_jcsp.sh
@@ -4,5 +4,9 @@
 sudo apt-get install openjdk-6-jdk
 wget http://www.cs.kent.ac.uk/projects/ofa/jcsp/jcsp-1.1-rc4.jar
 jar xf jcsp-1.1-rc4.jar
+rm jcsp-1.1-rc4.jar
+
+CLASSPATH=jcsp-1.1-rc4/jcsp.jar:jcsp-1.1-rc4/jcsp-demos-util.jar
+export CLASSPATH
 
 echo "Installation JCSP terminée"
