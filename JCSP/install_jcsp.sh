5a9c851d7f8f64e43b534c927dba47b5122a5773
Cle <clement@ubuntu.home>
2011-03-24 11:19:04 +0100

    Scripts d'install et de desinstall de JCSP

--- JCSP/install_jcsp.sh
+++ JCSP/install_jcsp.sh
@@ -9,6 +9,8 @@ wget http://www.cs.kent.ac.uk/projects/ofa/jcsp/jcsp-1.1-rc4.jar
 jar xf jcsp-1.1-rc4.jar
 rm jcsp-1.1-rc4.jar
 
-echo 'export CLASSPATH=$PATH:$.:~/JCSP/jcsp-1.1-rc4/jcsp.jar:~/JCSP/jcsp-1.1-rc4/jcsp-demos-util.jar' >> ~/.bashrc
+echo 'export JCSP_HOME=$HOME/JCSP/jcsp-1.1-rc4' >> ~/.bashrc
+
+source ~/.bashrc
 
 echo "Installation JCSP terminée"
